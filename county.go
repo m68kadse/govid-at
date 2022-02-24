b31eb53ed4a1f75268c931d59524f56951e7c727
Nicole Molly Benkö <crash@crashc.at>
2022-02-24 10:43:16 +0100

    add record constructors for County and Municipality

--- county.go
+++ county.go
@@ -1,11 +1,50 @@
 package govidat
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type County struct {
-	id             string
+	Id             string
 	Name           string
 	Residents      int64
 	Cases          int64
 	Deaths         int64
-	Incidence7d    int64
+	Cases7d        int64
 	Municipalities []Municipality
+}
+
+func countyFromRecord(rec []string) (*County, error) {
+	if len(rec) != 6 {
+		return nil, fmt.Errorf("record has incorrect number of fields: %v", rec)
+	}
+	county := new(County)
+
+	//record format:
+	//Name;id;Residents;Cases;Deaths;Incidence7d
+	county.Name = rec[0]
+	county.Id = rec[1]
+	if residents, err := strconv.ParseInt(rec[2], 10, 64); err != nil {
+		return nil, fmt.Errorf("cannot parse record, residents not a valid integer: %v", rec)
+	} else {
+		county.Residents = residents
+	}
+	if cases, err := strconv.ParseInt(rec[3], 10, 64); err != nil {
+		return nil, fmt.Errorf("cannot parse record, cases not a valid integer: %v", rec)
+	} else {
+		county.Cases = cases
+	}
+	if deaths, err := strconv.ParseInt(rec[4], 10, 64); err != nil {
+		return nil, fmt.Errorf("cannot parse record, deaths not a valid integer: %v", rec)
+	} else {
+		county.Deaths = deaths
+	}
+	if cases7d, err := strconv.ParseInt(rec[5], 10, 64); err != nil {
+		return nil, fmt.Errorf("cannot parse record, 7 day cases not a valid integer: %v", rec)
+	} else {
+		county.Cases7d = cases7d
+	}
+
+	return county, nil
 }
--- municipality.go
+++ municipality.go
@@ -1,17 +1,70 @@
 package govidat
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 )
 
 type Municipality struct {
-	id                 string
-	Updated            time.Time
-	Name               string
-	Population         int64
-	Dose1              int64
-	Dose2              int64
-	Dose3              int64
-	Certs              int64
-	Percent_vaccinated float64
+	Id                string
+	Updated           time.Time
+	Name              string
+	Population        int64
+	Dose1             int64
+	Dose2             int64
+	Dose3             int64
+	Certs             int64
+	PercentVaccinated float64
+}
+
+const (
+	timestampFormat = "2006-01-02T15:04:05-07:00"
+)
+
+func municipalityFromRecord(rec []string) (*Municipality, error) {
+	if len(rec) != 9 {
+		return nil, fmt.Errorf("record has incorrect number of records: %v", rec)
+	}
+	muni := new(Municipality)
+	//record format:
+	//Updated, Id, Name, Population, Dose1, Dose2, Dose3, Certs, PercentVaccinated
+	if updated, err := time.Parse(timestampFormat, rec[0]); err != nil {
+		return nil, fmt.Errorf("record has invalid timestamp: %v", rec)
+	} else {
+		muni.Updated = updated
+	}
+	muni.Id = rec[1]
+	muni.Name = rec[2]
+	if population, err := strconv.ParseInt(rec[3], 10, 64); err != nil {
+		return nil, fmt.Errorf("population is not a valid integer: %v", rec)
+	} else {
+		muni.Population = population
+	}
+	if d1, err := strconv.ParseInt(rec[4], 10, 64); err != nil {
+		return nil, fmt.Errorf("dose1 is not a valid integer: %v", rec)
+	} else {
+		muni.Dose1 = d1
+	}
+	if d2, err := strconv.ParseInt(rec[5], 10, 64); err != nil {
+		return nil, fmt.Errorf("dose2 is not a valid integer: %v", rec)
+	} else {
+		muni.Dose2 = d2
+	}
+	if d3, err := strconv.ParseInt(rec[6], 10, 64); err != nil {
+		return nil, fmt.Errorf("dose3 is not a valid integer: %v", rec)
+	} else {
+		muni.Dose3 = d3
+	}
+	if certs, err := strconv.ParseInt(rec[7], 10, 64); err != nil {
+		return nil, fmt.Errorf("certs is not a valid integer: %v", rec)
+	} else {
+		muni.Certs = certs
+	}
+	if percent_vax, err := strconv.ParseFloat(rec[8], 64); err != nil {
+		return nil, fmt.Errorf("percentVaccinated is not a valid float: %v", rec)
+	} else {
+		muni.PercentVaccinated = percent_vax
+	}
+	return muni, nil
 }
